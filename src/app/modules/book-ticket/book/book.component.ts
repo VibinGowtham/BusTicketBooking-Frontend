bd905cea12fbfb4453a87f14b261bb559737b645
VibinGowtham <vibingowtham.cs18@bitsathy.ac.in>
2022-05-07 20:11:43 +0530

    [Added] Bus details layout

--- src/app/modules/book-ticket/book/book.component.ts
+++ src/app/modules/book-ticket/book/book.component.ts
@@ -8,42 +8,41 @@ import { LocationService } from 'src/app/services/locationServices/location.serv
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
-  cities!:any
-city!:FormGroup
+  date!: any
+  busDetails!: any
+  cities!: any
+  city!: FormGroup
 
-  sendData() {
-    const{boardingLocation,destinationLocation,departureTime}=this.city.value
-    if(boardingLocation===destinationLocation) alert("Enter valid locations")
-    this.city.value.departureTime=parseInt(departureTime.slice(8))%7
+  sendData(): any {
+     const { boardingLocation, destinationLocation, departureDate } = this.city.value
+    if (boardingLocation === destinationLocation || departureDate === undefined) alert("Enter valid locations")
+    this.city.value.departureDate = parseInt(departureDate.slice(8)) % 7
+    console.log(parseInt(departureDate.slice(8)) % 7);
 
-    console.log(boardingLocation);
-    console.log(destinationLocation);
-    console.log(this.city.value.departureTime);
+    this.locationService.post('bus/filterBus', this.city.value).subscribe(data => {
 
-    this.locationService.post('bus/filterBus',this.city.value).subscribe(data=>{
-      console.log(data);
-      
+      this.busDetails = data
+      this.date=new Date().toDateString()
+      console.log(this.busDetails);
     })
   }
 
   constructor(private locationService: LocationService) { }
 
   ngOnInit(): void {
-   
-    
-    this.cities=[]
+    this.cities = []
     this.city = new FormGroup({
-      boardingLocation: new FormControl('',Validators.required),
-      destinationLocation: new FormControl('',Validators.required),
-      departureTime:new FormControl('',Validators.required),
+      boardingLocation: new FormControl('', Validators.required),
+      destinationLocation: new FormControl('', Validators.required),
+      departureDate: new FormControl('', Validators.required),
     })
     console.log(this.city.value);
 
-    this.locationService.get('city/getAllCities').subscribe(data=>{
-      for(let i=0;i<data.totalCities;i++){
-         this.cities.push(data.cities[i].city)
-     }
-      
+    this.locationService.get('city/getAllCities').subscribe(data => {
+      for (let i = 0; i < data.totalCities; i++) {
+        this.cities.push(data.cities[i].city)
+      }
+
     })
   }
 
